614b46f16b6abea9586593f39bbb1c1222b4cf04
writ3io <winguexpressdev@gmail.com>
2020-03-20 22:06:32 +0200

    create new interview form

--- lib/forms/create.dart
+++ lib/forms/create.dart
@@ -1,6 +1,5 @@
-import 'package:absapp/screens/home.dart';
-import 'package:absapp/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NewInterViewForm extends StatefulWidget {
   @override
@@ -8,49 +7,81 @@ class NewInterViewForm extends StatefulWidget {
 }
 
 class _NewInterviewFormState extends State<NewInterViewForm> {
-  
   final _formKey = GlobalKey<FormState>();
+  final Map<String, dynamic> formData = {'q1': null, 'q2': null};
+  final _q1 = TextEditingController();
+  final _q2 = TextEditingController();
   bool loading = false;
+ 
+  Widget _buildQ1Field() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      validator: (value) {
+        if (value.isEmpty) return 'Please enter some text';
+        return null;
+      },
+      controller: _q1,
+      //decoration: InputDecoration(labelText: 'Email'),
+    );
+  }
 
-    void _submitForm() async {
-      if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a Snackbar.
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Processing Data')));
-                }
+  Widget _buildQ2Field() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      validator: (value) {
+        if (value.isEmpty) return 'Please enter some text';
+        return null;
+      },
+      controller: _q2,
+      //decoration: InputDecoration(labelText: 'Email'),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return RaisedButton(
+      child: Text("Submit"),
+      onPressed: _submitForm,
+    );
+  }
+
+  void _submitForm() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      formData['q1'] = _q1.text;
+      formData['q2'] = _q2.text;
+      print(formData);
+      Scaffold.of(context).showSnackBar(
+          SnackBar(content: Text('Processing Data ' + formData['q1'])));
     }
+  }
 
   @override
   Widget build(BuildContext context) {
-     
-    return SafeArea(child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Form(
-          key  : _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 12),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(
-                width: 500,
-                child: RaisedButton(
-                  child: Text("Submit"),
-                  onPressed: _submitForm,
-                ),
-              )
-            ],)
-        )
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Name of the Dairy Corperative Union you belong to',style: Theme.of(context).textTheme.headline5.copyWith(),),
+                _buildQ1Field(),
+                SizedBox(height: 5),
+                Text('Name of the Dairy Corpaskas',style: Theme.of(context).textTheme.headline5.copyWith(),),
+                _buildQ2Field(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 500,
+                      child: _buildSubmitButton(),
+                    )
+                  ],
+                )
+              ],
+            ))
       ],
-    ));
-        
+    );
   }
-
 }
